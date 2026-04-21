ccf507443339bae8d414eca32d771a2d8795dc2a
Jaime Daniel <jaimeadanielfilho@hotmail.com>
2026-04-21 14:49:35 -0300

    feat(trabalhos/t2): refactor board representation to use bitmasks for pieces

--- trabalhos/t2/src/board.py
+++ trabalhos/t2/src/board.py
@@ -1,26 +1,36 @@
 import copy
-from typing import List, Iterable
+from typing import Iterable
 
 from player import *
 from position import *
 
 class Board:
     size: int
-    pieces: List[List[Player | None]]
+    white_pieces: int
+    black_pieces: int
     
     def __init__(self, size: int):
         self.size = size
-        self.pieces = [[None] * size for _ in range(size)]
+        self.white_pieces = 0
+        self.black_pieces = 0
     
     def with_piece(self, row: int, col: int, player: Player | None):
         return self.with_pieces([Position(row, col)], player)
     
     def with_pieces(self, positions: Iterable[Position], player: Player | None):
         new_board = Board(self.size)
-        new_board.pieces = copy.deepcopy(self.pieces)
+        new_board.white_pieces = self.white_pieces
+        new_board.black_pieces = self.black_pieces
         
         for pos in positions:
-            new_board.pieces[pos.row][pos.col] = player
+            bitmask = self.piece_bitmask(pos.row, pos.col)
+            
+            if player == Player.WHITE:
+                new_board.white_pieces |= bitmask
+                new_board.black_pieces &= ~bitmask
+            elif player == Player.BLACK:
+                new_board.black_pieces |= bitmask
+                new_board.white_pieces &= ~bitmask
         
         return new_board
     
@@ -29,10 +39,13 @@ class Board:
         
         for row in range(self.size):
             for col in range(self.size):
-                if self.pieces[row][col] == player:
+                if self[row, col] == player:
                     count += 1
                 
         return count
+    
+    def piece_bitmask(self, row: int, col: int):
+        return 1 << (row * self.size + col)
     
     def __getitem__(self, key: tuple[int, int]):
         row, col = key
@@ -43,5 +56,11 @@ class Board:
         if col < 0 or col >= self.size:
             return None
         
-        return self.pieces[row][col]
-    
+        bitmask = self.piece_bitmask(row, col)
+        
+        if self.white_pieces & bitmask != 0:
+            return Player.WHITE
+        elif self.black_pieces & bitmask != 0:
+            return Player.BLACK
+        else:
+            return None
